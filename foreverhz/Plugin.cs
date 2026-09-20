90bf340528d528e31864ba5cf1722920342023f2
iiDkRemastered <asus.noah@outlook.com>
2026-09-20 02:10:23 +0100

    Add files via upload

---
+++ foreverhz/Plugin.cs
@@ -0,0 +1,47 @@
+﻿using BepInEx;
+using GorillaNetworking;
+using HarmonyLib;
+using System;
+using System.Threading;
+using UnityEngine;
+using Valve.VR;
+
+namespace ForeverHz
+{
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static Plugin instance;
+        public int HZ = 60;
+        public bool gripOnly = false;
+        void Start() =>
+            instance = this;
+
+        void OnGUI()
+        {
+            HZ = (int)GUI.HorizontalSlider(new Rect(4f, 4f, 400f, 40f), HZ, 0f, 150f);
+            GUI.Label(new Rect(414f, 1f, 144f, 60f), "HZ: " + HZ.ToString());
+            GUI.Label(new Rect(4f, 40f, 900f, 40f), "HZ slider by @goldentrophy");
+            gripOnly = GUI.Toggle(new Rect(300f, 40f, 1080f, 40f), gripOnly, "Right Grip");
+        }
+
+        private static float lastTime;
+        void Update()
+        {
+            if (gripOnly && !ControllerInputPoller.instance.rightGrab)
+                return;
+
+            float targetDelta = 1f / HZ;
+            float elapsed = Time.realtimeSinceStartup - lastTime;
+
+            if (elapsed < targetDelta)
+            {
+                int sleepMs = Mathf.FloorToInt((targetDelta - elapsed) * 1000);
+                if (sleepMs > 0)
+                    Thread.Sleep(sleepMs);
+            }
+
+            lastTime = Time.realtimeSinceStartup;
+        }
+    }
+}
---
+++ foreverhz/README.md
@@ -0,0 +1,15 @@
+# ForeverHz
+A mod for Gorilla Tag to give you higher or lower FPS
+
+---
+
+## Usage
+A GUI should pop up on your screen with a slider and other settings. Turn up or down the slider to raise or lower your HZ, and you're good to go!
+
+## Installation
+
+- Download the release from [here](https://github.com/iiDk-the-actual/ForeverHz/releases/latest)
+- Drag the file `ForeverHz.dll` to your plugins folder
+- Start Gorilla Tag if needed
+
+> This product is not affiliated with Gorilla Tag or Another Axiom LLC and is not endorsed or otherwise sponsored by Another Axiom LLC. Portions of the materials contained herein are property of Another Axiom LLC. © 2025 Another Axiom LLC.
